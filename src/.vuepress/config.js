1a34e0d9fad850407e5fbbbae56f12a410a88b82
Patrick Roy <patroytall@gmail.com>
2020-12-13 05:38:18 -0500

    LinkedIn added

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -7,18 +7,10 @@ module.exports = {
    */
   description: description,
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['script', { src: 'https://patrickroy.me/__/firebase/8.1.1/firebase-app.js' }],
-    ['script', { src: 'https://patrickroy.me/__/firebase/8.1.1/firebase-analytics.js' }],
-    ['script', { src: 'https://patrickroy.me/__/firebase/init.js' }]
   ],
 
   /**
@@ -41,10 +33,6 @@ module.exports = {
         text: 'Config',
         link: '/config/'
       },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
     ],
     sidebar: {
       '/guide/': [
@@ -66,5 +54,23 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      'social-share',
+      {
+        networks: ['linkedin', 'email'],
+        email: 'patroytall@gmail.com',
+        noGlobalSocialShare: true,
+        extendsNetworks: {
+          linkedin: {
+            sharer:
+              'https://www.linkedin.com/in/patrick-roy-0ba24973/',
+            type: 'popup',
+            color: '#1786b1',
+            icon:
+              '<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d="M910.336 0H113.664A114.005333 114.005333 0 0 0 0 113.664v796.672A114.005333 114.005333 0 0 0 113.664 1024h796.672A114.005333 114.005333 0 0 0 1024 910.336V113.664A114.005333 114.005333 0 0 0 910.336 0zM352.256 796.330667H207.189333V375.466667h145.066667z m-72.021333-477.866667a77.824 77.824 0 0 1-81.237334-74.069333A77.824 77.824 0 0 1 280.234667 170.666667a77.824 77.824 0 0 1 81.237333 73.728 77.824 77.824 0 0 1-81.237333 73.386666z m582.314666 477.866667H716.8v-227.669334c0-46.762667-18.432-93.525333-73.045333-93.525333a84.992 84.992 0 0 0-81.237334 94.549333v226.304h-140.629333V375.466667h141.653333v60.757333a155.989333 155.989333 0 0 1 136.533334-71.338667c60.416 0 163.498667 30.378667 163.498666 194.901334z" /></svg>',
+          },
+        }
+      },
+    ],
   ]
 }
