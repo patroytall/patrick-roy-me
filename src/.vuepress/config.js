e8031fc862e0717d54d7d8ff41cdfd19a9a3a9df
Patrick Roy <patroytall@gmail.com>
2020-12-31 13:59:22 -0500

    Fix search

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -42,9 +42,6 @@ module.exports = {
     }
   },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
