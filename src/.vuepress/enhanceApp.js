623b9684c7ec1b9a7d8bf0df452c9be4a73ab0f5
Patrick Roy <patroytall@gmail.com>
2021-01-03 05:53:44 -0500

    Test analytics

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,14 +1,25 @@
-/**
- * Client app enhancement file.
- *
- * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
- */
+function loadJavaScriptAsync(file, onload) {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = file;
+  script.onload = onload();
+  document.body.appendChild(script);
+}
+
+// ["script", {src:"https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"}],
+// ["script", {src:"https://www.gstatic.com/firebasejs/8.2.1/firebase-analytics.js"}],
+// ["script", {src:"/__/firebase/init.js"}],
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  siteData, // site metadata
+  isServer
 }) => {
-  // ...apply enhancements for the site.
+  if (!isServer) {
+    loadJavaScriptAsync("/__/firebase/8.2.1/firebase-app.js", 
+      () => loadJavaScriptAsync("/__/firebase/8.2.1/firebase-analytics.js",
+        () => loadJavaScriptAsync("/__/firebase/init.js")));
+  }
 }
