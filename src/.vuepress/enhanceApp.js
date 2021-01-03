85510b92aba6e4eb3a005b8f529eaeb213d4d523
Patrick Roy <patroytall@gmail.com>
2021-01-03 06:06:23 -0500

    Analytics try #2

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -2,13 +2,11 @@ function loadJavaScriptAsync(file, onload) {
   const script = document.createElement("script");
   script.type = "text/javascript";
   script.src = file;
-  script.onload = onload();
+  if (onload) {
+    script.onload = onload;
+  }
   document.body.appendChild(script);
 }
-
-// ["script", {src:"https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"}],
-// ["script", {src:"https://www.gstatic.com/firebasejs/8.2.1/firebase-analytics.js"}],
-// ["script", {src:"/__/firebase/init.js"}],
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -18,8 +16,9 @@ export default ({
   isServer
 }) => {
   if (!isServer) {
-    loadJavaScriptAsync("/__/firebase/8.2.1/firebase-app.js", 
+    loadJavaScriptAsync("/__/firebase/8.2.1/firebase-app.js",
       () => loadJavaScriptAsync("/__/firebase/8.2.1/firebase-analytics.js",
-        () => loadJavaScriptAsync("/__/firebase/init.js")));
+        () => loadJavaScriptAsync("/__/firebase/init.js"))
+    );
   }
 }
