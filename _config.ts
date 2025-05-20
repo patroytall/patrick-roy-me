2052f2a45b0e2fd4edf0e0c5913886f907079dc5
Patrick Roy <patroytall@gmail.com>
2025-05-19 20:43:59 -0400

    Example site

--- _config.ts
+++ _config.ts
@@ -1,5 +1,7 @@
 import lume from "lume/mod.ts";
 
-const site = lume();
+const site = lume({
+  src: "./src",
+});
 
 export default site;
---
+++ src/index.md
@@ -0,0 +1,5 @@
+# Welcome to my website
+
+This is my first page using **Lume,** a static site generator for Deno.
+
+I hope you enjoy it.
